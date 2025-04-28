1390ab44c9e40ea9d2928ac357709f1efdc091ff
Hao Wu <45955330+oxygen26@users.noreply.github.com>
2025-04-28 12:17:45 +0800

    1

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ model = AutoModel(
     model="iic/speech_seaco_paraformer_large_asr_nat-zh-cn-16k-common-vocab8404-pytorch",
     vad_model="iic/speech_fsmn_vad_zh-cn-16k-common-pytorch",
     punc_model="iic/punc_ct-transformer_zh-cn-common-vocab272727-pytorch",
-    device='gpu'
+    device='cpu'
 )
 
 r = os.getenv('RULE', 'on')
