c01c028080bb7411cf7660cc770f956cc90eeead
Hao Wu <45955330+oxygen26@users.noreply.github.com>
2025-04-28 10:10:48 +0800

    1

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ root_log.setLevel(logging.WARNING)
 
 # model_conf
 model = AutoModel(
-    model="iic/SenseVoiceSmall",#speech_seaco_paraformer_large_asr_nat-zh-cn-16k-common-vocab8404-pytorch",
+    model="speech_seaco_paraformer_large_asr_nat-zh-cn-16k-common-vocab8404-pytorch",
     vad_model="iic/speech_fsmn_vad_zh-cn-16k-common-pytorch",
     punc_model="iic/punc_ct-transformer_zh-cn-common-vocab272727-pytorch",
     device='gpu'
